a42e7d3e9eb6853a57e9e543618965c097fd9ab6
Zach Stoebner <zstoebner25@gmail.com>
2019-08-08 12:46:33 -0500

    admin solution to 8-7-2019, my solution to 8-8-2019

--- 8-7-2019.py
+++ 8-7-2019.py
@@ -9,6 +9,9 @@ or modulus operators. Return the quotient
 as an integer, ignoring the remainder.
 
 """
+
+import time
+
 #know that inexact division and multiplication can be implemented with recursion
 def inexactDivision(numerator,denominator):
 
@@ -19,8 +22,41 @@ def inexactDivision(numerator,denominator):
     else:
         return 1 + inexactDivision(difference,denominator)
 
+t1 = time.time()
 print(inexactDivision(4,2))
 print(inexactDivision(10,3))
 print(inexactDivision(3,10))
 print(inexactDivision(20,1))
 print(inexactDivision(1,20))
+print(inexactDivision(31,3))
+t2 = time.time()
+print("Time: ", t2-t1)
+
+###SOLUTION GIVEN BY DCP
+#More complicated, O(N) where N is the number of bits in x/y, slightly faster
+def divide(x, y):
+    if y == 0:
+        raise ZeroDivisionError('division by zero')
+
+    quotient = 0
+    power = 32           # Assume 32-bit integer
+    yPower = y << power  # Initial y^d value is y^32
+    remainder = x        # Initial remainder is x
+    while remainder >= y:
+        while yPower > remainder:
+            yPower >>= 1
+            power -= 1
+        quotient += 1 << power
+        remainder -= yPower
+
+    return quotient
+
+t3 = time.time()
+print(divide(4,2))
+print(divide(10,3))
+print(divide(3,10))
+print(divide(20,1))
+print(divide(1,20))
+print(divide(31,3))
+t4 = time.time()
+print("Time: ",t4-t3)
---
+++ 8-8-2019.py
@@ -0,0 +1,82 @@
+"""
+Author: Zach Stoebner
+Created on: 8-8-2019
+Descrip:
+
+Determine whether a tree is a valid binary search tree.
+
+"""
+#know that BSTs are naturally recursive
+#implementing a postorder check
+def checkBST(tree):
+
+    def isBST(root):
+        left_valid = True
+        right_valid = True
+
+        #checking left subtree
+        if root.left != None:
+            left_valid = (isBST(root.left) and root.left.data <= root.data)
+
+        #checking right subtree
+        if root.right != None:
+            right_valid = (isBST(root.right) and root.right.data >= root.data)
+
+        return (left_valid and right_valid)
+
+    return isBST(tree.root)
+
+
+class Node(object):
+
+    #constructor
+    def __init__(self,data,left=None,right=None):
+        self.data = data
+        self.left = left
+        self.right = right
+
+class BST(object):
+
+    #constructor
+    def __init__(self,root=Node(0)):
+        self.root = root
+
+    def insert(self,node,root=None):
+
+        #setting root
+        if root == None:
+            root = self.root
+
+        #inserting node
+        if (root.left == None and node.data <= root.data):
+            root.left = node
+        elif (root.right == None and node.data >= root.data):
+            root.right = node
+        else:
+            if (node.data <= root.data):
+                self.insert(node,root.left)
+            else:
+                self.insert(node,root.right)
+
+#creating test BSTs
+
+#valid BST
+bst1 = BST()
+bst1.insert(Node(-1))
+bst1.insert(Node(1))
+print(checkBST(bst1))
+bst1.insert(Node(-3))
+bst1.insert(Node(-2))
+print(checkBST(bst1))
+
+#not a BST
+bad = Node(0,Node(-1,Node(6)),Node(5,Node(4),Node(4)))
+badBST = BST(bad)
+print(checkBST(badBST))
+
+#unbalanced BST, still valid
+unbalanced = BST()
+unbalanced.insert(Node(-1))
+unbalanced.insert(Node(1))
+unbalanced.insert(Node(0))
+print(checkBST(unbalanced))
