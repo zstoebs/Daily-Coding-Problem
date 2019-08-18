3f9f5450764a011580360cfb62b829ecf07f1eb5
Zach Stoebner <zstoebner25@gmail.com>
2019-08-18 16:06:11 -0500

    added correct hash sol to 8-18

--- 8-18-2019.py
+++ 8-18-2019.py
@@ -27,7 +27,7 @@ def easy_solution(ints):
     length = len(ints)
 
     count = 0
-    max = count
+    mx = count
     prev = None
     for int in sorted(ints): #sort is O(nlogn) time
 
@@ -39,54 +39,19 @@ def easy_solution(ints):
             # if consecutive, increment
             if int == prev+1:
                 count += 1
+                mx = max(count,mx)
 
-                #if new max, set
-                if count > max:
-                    max = count
             #if not consecutive, reset
             else:
                 count = 1
             prev = int
-    return max
+    return mx
 
 ### TESTS
 ints = [100, 4, 200, 1, 3, 2]
 print(easy_solution(ints)) #4
 ints = [1,2,3,4,100,101,102,103,104,105,106,107]
 print(easy_solution(ints)) #8
-
-#longest_sequence
-#Note: returns length of longest consecutive sequence of elements
-#Complexity: O(nlogn) time + O(n) space
-def longest_sequence(ints):
-
-    length = len(ints)
-
-    #helper
-    #Note: returns count of largest sequence
-    def helper(arr,ind,count=1):
-
-        #getting next index
-        next_ind = binary_search(arr,arr[ind]+1,0,length-1)
-
-        #if end of sequence, return
-        if next_ind is None:
-            return count
-        #else increment and continue
-        else:
-            count +=1
-            return helper(arr,next_ind,count)
-
-
-    counts = []
-    for i in range(length):
-        counts.append(helper(ints,i))
-    return max(counts)
-
-### TESTS
-ints = [100, 4, 200, 1, 3, 2]
-print()
-print(longest_sequence(ints)) #1, should be 4
 
 #binary_search
 #Note: returns index
@@ -116,3 +81,62 @@ print(binary_search(ints,0,0,len(ints)-1)) #0
 print(binary_search(ints,8,0,len(ints)-1)) #None
 print(binary_search(ints,6,0,len(ints)-1)) #6
 #bin search passes tests
+
+#longest_sequence
+#Note: returns length of longest consecutive sequence of elements
+#Complexity: O(nlogn) time + O(n) space
+### DOESN'T WORK BECAUSE ARRAY ISN'T SORTED FOR BIN SEARCH
+def longest_sequence(ints):
+
+    length = len(ints)
+
+    #helper
+    #Note: returns count of largest sequence
+    def helper(ind,count=1):
+
+        #getting next index
+        next_ind = binary_search(ints,ints[ind]+1,0,length-1)
+        print(next_ind)
+
+        #if end of sequence, return
+        if next_ind is None:
+            return count
+        #else increment and continue
+        else:
+            count += 1
+            return helper(next_ind,count)
+
+
+    count = 0
+    for i in range(length):
+        count = max(count,helper(i))
+    return count
+
+### TESTS
+ints = [100, 4, 200, 1, 3, 2]
+print()
+print(longest_sequence(ints)) #1, should be 4
+
+#hash_sequence
+#Note: hashes values, and returns max count
+#Complexity: O(n) time + O(n) space
+# https://www.geeksforgeeks.org/longest-consecutive-subsequence/
+def hash_sequence(ints):
+
+    #python sets use hashes for constant time lookup
+    hashes = set(ints)
+    count = 0
+
+    for int in ints:
+
+        cur = int+1
+        while cur in hashes:
+            cur += 1
+
+        count = max(count,cur-int)
+
+    return count
+
+ints = [100, 4, 200, 1, 3, 2]
+print()
+print(hash_sequence(ints))
