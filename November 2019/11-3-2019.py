878e818ff78d2950020f22067c42bbeaa4a11626
Zach Stoebner <zstoebner25@gmail.com>
2019-11-03 22:03:01 -0600

    updated my sol to 11-3

--- November 2019/11-3-2019.py
+++ November 2019/11-3-2019.py
@@ -16,11 +16,49 @@ Given s1 = foo and s2 = bar, return false since the o cannot map to two characte
 def injection(s1,s2):
 
     set1 = set()
-    set1 = set()
-
+    set2 = set()
+    cnt1 = list()
+    cnt2 = list()
     for c in s1:
         set1.add(c)
     for c in s2:
-        set1.add(c)
+        set2.add(c)
 
-    return len(set1) <= len(set2)
+    # checking PHP
+    l1 = len(set1)
+    l2 = len(set2)
+    if l2 < l1:
+        return False
+
+    for c in set1:
+        cnt1.append(tuple([c,s1.count(c)]))
+    for c in set2:
+        cnt2.append(tuple([c,s2.count(c)]))
+
+    pro = lambda x: x[1]
+    cnt1.sort(key=pro)
+    cnt2.sort(key=pro)
+    print(cnt1,cnt2)
+
+    for let in cnt1:
+        while l2 > 0 and cnt2[0][1] < let[1]:
+            cnt2.pop(0)
+            l2 -= 1
+        if l2 > 0:
+            cnt2.pop(0)
+            l2 -= 1
+            l1 -= 1
+
+        # check PHP
+        if l2 < l1:
+            return False
+
+    return True
+
+### TESTS
+s1 = "abc"
+s2 = "bcd"
+print(injection(s1,s2))
+s1 = "foo"
+s2 = "bar"
+print(injection(s1,s2))
